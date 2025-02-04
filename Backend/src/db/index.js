6aff1483b9fc8ee89682ec7c70eddf828a70cfb8
Maheen Fatma <goodmaheen@gmail.com>
2025-02-04 23:06:01 +0530

    dotenv integration

--- Backend/src/db/index.js
+++ Backend/src/db/index.js
@@ -4,7 +4,11 @@ import { DB_NAME } from "../constants"
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect (`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\n Mongo DB connected ${connectionInstance.connection.host}`);
     } catch (error) {
-        
+        console.log("Error: ",error);
+        process.exit(1)
     }
 }
+
+export default connectDB
