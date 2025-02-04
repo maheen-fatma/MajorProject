281f37dc54ef6eedd29ae38a11976b61062ec665
Maheen Fatma <goodmaheen@gmail.com>
2025-02-04 23:54:22 +0530

    Utility functions

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,4 +7,13 @@ app.use(cors({
     credentials: true
 }))
 
+app.use(express.json({
+    limit: "16kb"
+}))
+
+app.use(express.urlencoded({extended:true, limit:"16kb"}))
+
+app.use(cookieParser())
+
+app.use(express.statis("public"))
 export {app}
