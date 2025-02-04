ca4ed7e28cdd2fcabbb90aa1c6b1636d19ce8533
Maheen Fatma <goodmaheen@gmail.com>
2025-02-04 23:30:56 +0530

    Parser integration

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,3 +1,10 @@
 import express from "express"
-const app = expres()
+import cors from "cors"
+
+const app = express()
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
 export {app}
