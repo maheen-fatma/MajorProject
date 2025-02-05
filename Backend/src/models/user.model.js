b478529856f1fcbe6a685eaf46167bfd97191f1c
Maheen Fatma <goodmaheen@gmail.com>
2025-02-05 14:00:18 +0530

    hashing password before saving

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import bcrypt from "bcrypt"
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true, lowercase:true },
     email: { type: String, required: true, unique: true, lowercase:true },
@@ -7,4 +8,10 @@ const userSchema = new mongoose.Schema({
     password: { type: String, required: true },
     refreshToken: { type: String, default: '' },
 },{timestamps:true})
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password"))
+        return next()
+    this.password = await bcrypt.hash(this.password,10)
+    next()
+})
 export const user = mongoose.model("User",userSchema)
