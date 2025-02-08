c117cc45d0bd8539f18aa5a60e44e232e1bdd712
Maheen Fatma <goodmaheen@gmail.com>
2025-02-08 19:48:45 +0530

    Logout controller and routes

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -112,7 +112,12 @@ const loginUser = asyncHandler ( async (req,res)=>{
     ))
 })
 
+const logoutUser = asyncHandler ( async (req,res)=>{
+
+})
+
 export {
     registerUser,
     loginUser,
+    logoutUser
 }
---
+++ Backend/src/middleware/auth.middleware.js
@@ -0,0 +1,14 @@
+import { asyncHandler } from "../utils/asyncHandler";
+import { ApiError } from "../utils/ApiError";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model";
+
+export const verifyJWT = asyncHandler (async(req,res)=>{
+    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+    if(!token) throw new ApiError(401, "Unauthorized request")
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+    const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+    if(!user) throw new ApiError(404, "User not found")
+    req.user = user
+    next()
+})
